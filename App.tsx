4fc82c5cb3705529ab7e3ec8de0e1825d86120b2
__frantisek_petko <oristgrey@gmail.com>
2021-11-25 09:44:12 +0100

    Typescript added

--- App.tsx
+++ App.tsx
@@ -1,21 +1,63 @@
-import { StatusBar } from 'expo-status-bar';
-import React, {useEffect, useState} from 'react';
+import React, {useEffect, useState, FC, ReactNode, ReactElement} from 'react';
 import { FlatList, SafeAreaView, StyleSheet, Text, View, Image, TouchableWithoutFeedback, Button } from 'react-native';
 import axios from 'axios';
+import {sortBy} from 'lodash';
 
-const Header = () => {
+interface Movie {
+  title: string,
+  episode_number: string,
+  main_characters: string[],
+  description: string,
+  poster: string,
+  hero_image: string
+}
+
+const Header:FC<void> = () => {
+  // style={styles.item}
   return <View style={styles.header}>
     <View style={styles.headerTitleWrapper}><Text style={styles.headerTitle}>StarWars </Text></View>
     </View>
 };
+/*
+const Item: FC = (movie:Movie) => {
+  console.log('itemd', movie);
+  let baseImageUrl = 'https://raw.githubusercontent.com/RyanHemrick/star_wars_movie_app/master/public/images/';
+  return <View style={styles.item}>
+    <Text style={styles.title}>{movie.title}</Text>
+    <Text style={styles.subtitle}>Episode number: {movie.episode_number}</Text>
+    <View style={styles.imageContainer}>
+      <Image source={{
+        uri: `${baseImageUrl}${movie.poster}`,
+        width: 250,
+        height: 250
+      }}/>
+      <Text style={{marginTop: 15}}>
+        {movie.description}
+      </Text>
+      <View style={styles.mainCharactersContainer}>
+        <Text style={styles.charactersHeading}>Main Characters</Text>
+        {movie?.main_characters?.map((itm:string, index:number) => <Text style={styles.mainCharactersText} key={index}>{itm}</Text>)}
+      </View>
 
-const Item = ({ item }) => {
+      <Image source={{
+        uri: `${baseImageUrl}${movie.hero_image}`,
+        width: 150,
+        height: 150
+      }}/>
+    </View>
+  </View>
+};
+*/
+
+const renderItem:FC<{item: Movie}> = ({item}) => {
+  //console.log('renderItem', item);
+  //return <Item movie={item}/>
   let baseImageUrl = 'https://raw.githubusercontent.com/RyanHemrick/star_wars_movie_app/master/public/images/';
   return <View style={styles.item}>
     <Text style={styles.title}>{item.title}</Text>
     <Text style={styles.subtitle}>Episode number: {item.episode_number}</Text>
     <View style={styles.imageContainer}>
-      <Image styles={styles.logo} source={{
+      <Image source={{
         uri: `${baseImageUrl}${item.poster}`,
         width: 250,
         height: 250
@@ -25,30 +67,25 @@ const Item = ({ item }) => {
       </Text>
       <View style={styles.mainCharactersContainer}>
         <Text style={styles.charactersHeading}>Main Characters</Text>
-        {item.main_characters.map((item, index) => <Text style={styles.mainCharactersText} key={index}>{item}</Text>)}
+        {item?.main_characters?.map((itm:string, index:number) => <Text style={styles.mainCharactersText} key={index}>{itm}</Text>)}
       </View>
 
-      <Image styles={styles.logo} source={{
+      <Image source={{
         uri: `${baseImageUrl}${item.hero_image}`,
         width: 150,
         height: 150
       }}/>
     </View>
-
   </View>
 };
 
-const renderItem = ({ item }) => {
-  return <Item item={item}/>
-};
+const App:FC<void> = () =>  {
 
-export default function App() {
+  const [sortItemsAscend, setSortItemsAscend] = useState<boolean>(true);
 
-  const [sortItemsAscend, setSortItemsAscend] = useState(true);
+  const [data, setData] = useState<Movie[]>([]);
 
-  const [data, setData] = useState([]);
-
-  const [loading, setLoading] = useState(true);
+  //console.log('datax', data);
 
   /*
   const sortItemsAscendOrDescend = () =>  {
@@ -68,42 +105,58 @@ export default function App() {
   }
   */
 
-  const sortItemsAscendOrDescend = () =>  {
-      
-    const copy = list => [...list];
 
-    console.log(sortItemsAscend ? "descending" : "ascending");
-    const items = copy(data).sort((a, b) => 
-      Number(sortItemsAscend ? b.episode_number : a.episode_number) - Number(sortItemsAscend ? a.episode_number : b.episode_number)
+  function sortAscendOrDescend(list: Movie[]):Movie[] {
+    const tempList:Movie[] = [...list];
+    if (sortItemsAscend) {
+      return tempList.sort(
+        (a: Movie, b: Movie) =>
+          Number(b.episode_number) - Number(a.episode_number)
+      );
+    }
+    return tempList.sort(
+      (a: Movie, b: Movie) =>
+        Number(a.episode_number) - Number(b.episode_number)
     );
+  }
+
+  const sortItems = () =>  {
+    
+    console.log(sortItemsAscend ? "descending" : "ascending");
+    /*const items = [...data].sort((a, b) => 
+      Number(sortItemsAscend ? b.episode_number : a.episode_number) - Number(sortItemsAscend ? a.episode_number : b.episode_number)
+    );*/
+    /*const items = [...list].sort((a, b) => 
+      Number(sortItemsAscend ? b.episode_number : a.episode_number) - Number(sortItemsAscend ? a.episode_number : b.episode_number)
+    );*/
+    const items:Movie[] = sortAscendOrDescend(data);
     setData(items);
-    setSortItemsAscend((prevState) => !prevState);
+    setSortItemsAscend((prevState:boolean) => !prevState);
   }
   
-
-  useEffect(async () => {
-    setLoading(true);
-    const data = await axios.get('https://raw.githubusercontent.com/RyanHemrick/star_wars_movie_app/master/movies.json');
-    setData(data.data.movies);
-    setLoading(false);
+  useEffect(() => {
+    (async () => {
+      const result = await axios.get('https://raw.githubusercontent.com/RyanHemrick/star_wars_movie_app/master/movies.json');
+      setData([...result.data.movies]);
+    })();
   }, []);
 
   return (
     <SafeAreaView style={styles.container}>
-      <Header style={styles.item}/>
+      <Header/>
       {data.length === 0   && <Text style={styles.loadingText}>Loading ...</Text>}
       {data.length > 0  && <FlatList
         data={data}
         extraData={sortItemsAscend}
         renderItem={renderItem}
-        keyExtractor={(item, index) => index}
+        keyExtractor={(item:Movie, index:number ) => {return String(index)} }
       />}
       <View style={styles.footerButtonContainer}>
         <Button
             title={sortItemsAscend ? "Sort Items Descent" : "Sort Items Ascend" }
             color="orange"
             accessibilityLabel="Tap to Decrypt Data"
-            onPress={sortItemsAscendOrDescend}
+            onPress={sortItems}
             />  
       </View>
     </SafeAreaView>
@@ -133,7 +186,6 @@ const styles = StyleSheet.create({
   },
   container: {
     flex: 1,
-    marginTop: StatusBar.currentHeight || 0,
   },
   item: {
     backgroundColor: '#f9c200',
@@ -204,3 +256,5 @@ const styles = StyleSheet.create({
       height: 45,
   }
 });
+
+export default App;
